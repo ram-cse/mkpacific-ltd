0796c838840adb853cf064ff28a6700ad3295be4
thanhdungvn <thanhdungvn@e6f55542-1be3-38e0-bcc8-b66e3d86884e>
2010-09-15 11:13:34 +0000

    đã định hình dc kien truc he thong

---
+++ trunk/07.SourceCode/08.MoneyPacificService/08.MoneyPacificService/BUS/StoreBUS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using _08.MoneyPacificService.DTO;
+
+namespace _08.MoneyPacificService.BUS
+{
+    public class StoreBUS
+    {
+        //public Money
+        
+    }
+}
---
+++ trunk/07.SourceCode/P6_CallWCFLibrary/P6_CallWCFLibrary/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace P6_CallWCFLibrary
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Service1Client sc = new Service1Client();
+            string message = sc.GetData(5000);
+            lblMessage.Content = message;
+            sc.Close();
+        }
+    }
+}
