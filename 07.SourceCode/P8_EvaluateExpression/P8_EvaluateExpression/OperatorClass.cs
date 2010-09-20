6d594efac7d7082fe3f8c36061f86920c243750e
thanhdungvn <thanhdungvn@e6f55542-1be3-38e0-bcc8-b66e3d86884e>
2010-09-20 11:01:48 +0000

    1 phần bản Tính expression (chưa load xml)

---
+++ 07.SourceCode/P8_EvaluateExpression/P8_EvaluateExpression/OperatorClass.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+namespace P8_EvaluateExpression
+{
+    class OperatorClass
+    {
+        internal char value;
+
+        internal int valuePrior()
+        {
+            int result = 0;
+            
+            switch (value)
+            {
+                case '+':
+                    result = 1;
+                    break;
+                case '-':
+                    result = 1;
+                    break;
+                case '*':
+                    result = 2;
+                    break;
+                case '/':
+                    result = 2;
+                    break;
+                case '%':
+                    result = 2;
+                    break;
+                case '^':
+                    result = 3;
+                    break;
+                case '(':
+                    result = int.MaxValue;
+                    break;
+                case ')':
+                    result = int.MinValue;
+                    break;
+                default:
+                    result = 0;
+                    break;
+            }
+
+          return result;  
+        }
+
+        internal double calculate(int x, int y)
+        {
+            double result = 0;
+
+            switch (value)
+            {
+                case '+':
+                    result = (double)(x + y);
+                    break;
+                case '-':
+                    result = (double)(x - y);
+                    break;
+                case '*':
+                    result = (double)(x * y);
+                    break;
+                case '/':
+                    result = (double)(x / y);
+                    break;
+                case '%':
+                    result = (double)(x % y);
+                    break;
+                case '^':
+                    result = (double)Math.Pow(x, y);
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
