819f2bc0e57c1c26a70916a6c340e1d634e096b8
thanhdungvn <thanhdungvn@e6f55542-1be3-38e0-bcc8-b66e3d86884e>
2010-09-20 11:01:48 +0000

    1 phần bản Tính expression (chưa load xml)

---
+++ trunk/07.SourceCode/P8_EvaluateExpression/P8_EvaluateExpression/ExpresstionClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P8_EvaluateExpression
+{
+    class ExpresstionClass
+    {
+        List<int> lstNumber;
+        List<OperatorClass> lstOperator;
+
+        internal static double evaluateExp(string expression)
+        {
+            return 1;
+        }
+    }
+}
