504102ce179f597e61ce4a3d69ecd0d666d9ce71
thanhdungvn <thanhdungvn@e6f55542-1be3-38e0-bcc8-b66e3d86884e>
2010-09-20 10:32:17 +0000

    các bản trươc add thiếu tập tin

---
+++ 07.SourceCode/P9_Coder/P9_Coder/Coder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P9_Coder
+{
+    class Coder
+    {
+        internal static int iKey01 = 19;
+        internal static int iKey02 = 83;
+
+        internal static string EnCode(string originString)
+        {
+            string resultString = "";
+
+            for (int i = 0; i < originString.Length; i++)
+            {
+                if (i % 2 == 0)
+                    resultString += (char)(originString[i] ^ iKey01);
+                else
+                    resultString += (char)(originString[i] ^ iKey02);
+            }
+
+            resultString = (char)iKey02 + resultString;
+            resultString = (char)iKey01 + resultString;
+
+            return resultString;
+        }
+
+        internal static string DeCode(string originString)
+        {
+            // 2 ký tự đầu tiên là khóa
+
+            int pKey01 = (int)originString[0];
+            int pKey02 = (int)originString[1];
+
+            string resultString = "";
+
+            for (int i = 2; i < originString.Length; i++)
+            {
+                if (i % 2 == 0)
+                    resultString += (char)(originString[i] ^ pKey01);
+                else
+                    resultString += (char)(originString[i] ^ pKey02);
+            }
+
+            return resultString;
+        }
+    }
+}
