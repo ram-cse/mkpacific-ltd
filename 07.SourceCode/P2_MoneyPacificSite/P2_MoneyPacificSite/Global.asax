1542f5945d9835d944ccd5ba08a7c717c5c461e7
thanhdungvn <thanhdungvn@e6f55542-1be3-38e0-bcc8-b66e3d86884e>
2010-10-08 09:24:28 +0000

    MoneyPacificSite: Value  & Detail on Internet    Nhâp số điện thoại, kiểm tra mua trong 24h thì gửi lại tin nhắn (thong wa webservice)

---
+++ 07.SourceCode/P2_MoneyPacificSite/P2_MoneyPacificSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="P2_MoneyPacificSite.MvcApplication" Language="C#" %>
---
+++ 07.SourceCode/P2_MoneyPacificSite/P2_MoneyPacificSite/ViewModels/CustomerBrowseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using P2_MoneyPacificSite.Models;
+
+
+namespace P2_MoneyPacificSite.ViewModels
+{
+    public class CustomerBrowseViewModel
+    {
+        public int NumberOfPacificCode { get; set; }
+        public List<PacificCode> PacificCodes { get; set; }
+    }
+}
