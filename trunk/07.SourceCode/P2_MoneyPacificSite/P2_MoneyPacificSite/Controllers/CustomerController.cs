40ddc25a249d857444443e70f3c64437e4b2e412
thanhdungvn <thanhdungvn@e6f55542-1be3-38e0-bcc8-b66e3d86884e>
2010-10-08 09:24:28 +0000

    MoneyPacificSite: Value  & Detail on Internet    Nhâp số điện thoại, kiểm tra mua trong 24h thì gửi lại tin nhắn (thong wa webservice)

---
+++ trunk/07.SourceCode/P2_MoneyPacificSite/P2_MoneyPacificSite/Controllers/CustomerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using P2_MoneyPacificSite.Models;
+using P2_MoneyPacificSite.ViewModels;
+
+namespace P2_MoneyPacificSite.Controllers
+{
+    public class CustomerController : Controller
+    {
+        //
+        // GET: /Customer/
+
+        public ActionResult Index()
+        {            
+            MoneyPacificDataContext db = new MoneyPacificDataContext();
+            var viewModel = db.Customers.Where(c => c.ID == 1).Single<Customer>();
+            return View(viewModel);
+        }
+
+        public ActionResult Browse(int id)
+        {
+            MoneyPacificDataContext db = new MoneyPacificDataContext();
+
+            List<PacificCode> lstPc = db.PacificCodes.Where(p => p.CustomerID == id).ToList<PacificCode>();
+            // Kiểm tra thông tin đăng nhập, truyền vào ID
+            var viewModel = new CustomerBrowseViewModel
+            {
+                PacificCodes = lstPc,
+                NumberOfPacificCode = lstPc.Count()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
