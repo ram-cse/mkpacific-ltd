9433f58884ccb804bf58d35b2474637bf07440b8
thanhdungvn <thanhdungvn@e6f55542-1be3-38e0-bcc8-b66e3d86884e>
2010-11-03 07:16:56 +0000

    up mot vi du khá hữu dụng về phân quyền

---
+++ trunk/07.SourceCode/PhanQuyenDemo/PhanQuyenDemo/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PhanQuyenDemo
+{
+    interface ICommand
+    {
+        void Execute();
+    }
+}
---
+++ trunk/07.SourceCode/PhanQuyenDemo/PhanQuyenDemo/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Reflection;
+
+namespace PhanQuyenDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IUser[] lstUser = new IUser[2];
+            lstUser[0] = new Guest();
+            lstUser[1] = new Administrator();
+
+            ICommand curCmd = new LockedCommand();
+
+            foreach (IUser u in lstUser)
+            {
+                Console.WriteLine(u.GetRoles());
+                string uRole = u.GetRoles();
+
+                MethodInfo curMethod = curCmd.GetType().GetMethod("Execute");
+                foreach(Attribute attr in curMethod.GetCustomAttributes(true))
+                {
+                    AuthorizeAttribute thuoctinh = (AuthorizeAttribute) attr;
+                    
+                    //if (thuoctinh.Roles == uRole)
+                    if(thuoctinh.Roles.Contains(uRole))
+                    {
+                        curCmd.Execute();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ban khong co quyen thuc thi lenh nay!...");
+                    }
+                }
+            }
+            
+        }
+    }
+
+    public interface IUser
+    {        
+        string GetRoles();
+    }
+
+    public class Guest : IUser
+    {
+        public string GetRoles()
+        {            
+            return this.GetType().Name;
+        }
+    }
+
+    public class Administrator  : IUser
+    {
+        public string GetRoles()
+        {
+            return this.GetType().Name;
+        }
+    }
+}
