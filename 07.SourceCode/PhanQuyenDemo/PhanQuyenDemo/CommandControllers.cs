4248a46fea10be219ea2ae112ffb6174e8ddbcb3
thanhdungvn <thanhdungvn@e6f55542-1be3-38e0-bcc8-b66e3d86884e>
2010-11-03 07:16:56 +0000

    up mot vi du khá hữu dụng về phân quyền

---
+++ 07.SourceCode/PhanQuyenDemo/PhanQuyenDemo/CommandControllers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PhanQuyenDemo
+{
+    public class BuyCommand : ICommand
+    {
+        [Authorize(Roles = "User")]
+        public void Execute()
+        {
+            Console.WriteLine("Lenh BUY, moi user deu co the truy xuat");
+        }
+    }
+
+    public class LockedCommand : ICommand
+    {
+        [Authorize(Roles="Administrator, Guest")]
+        public void Execute()
+        {
+            Console.WriteLine("Lenh LOCKED, chi co admin moi co the truy xuat");
+        }
+    }
+
+}
